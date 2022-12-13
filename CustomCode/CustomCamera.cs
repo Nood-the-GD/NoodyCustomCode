a8f3b7adf2256f96d3643ff7e0386244b38b1e6c
Tuong <tuongnt@vitalify.asia>
2022-12-13 11:41:58 +0700

    Update abtracMonoBehaviour.cs, MonoBehaviourInstance.cs and DelayCall.cs

--- CustomCode/CustomCamera.cs
+++ CustomCode/CustomCamera.cs
@@ -10,6 +10,15 @@ public class CustomCamera : MonoBehaviour
     #region Stats
     [SerializeField] float duration, magnitude;
     [SerializeField] float explodeMagnitude;
+    [SerializeField] float smoothTime;
+    [SerializeField] string targetTag;
+    Transform targetTransform;
+
+    [SerializeField] Vector3 offset;
+
+    [SerializeField] bool isFollow;
+    [SerializeField] bool isShake;
+    [SerializeField] bool isHeavyShake;
     #endregion
 
     public static CustomCamera InsCustomCamera;
@@ -18,12 +27,31 @@ public class CustomCamera : MonoBehaviour
     {
         if(InsCustomCamera == null) InsCustomCamera = this;
     }
+
+    private void Update()
+    {
+        if (isShake) Shake();
+        if (isHeavyShake) HeaveShake();
+    }
+
+    private void LateUpdate()
+    {
+        if (isFollow) FollowPlayer();
+    }
+
+    void FollowPlayer()
+    {
+        if (!targetTransform && GameObject.FindGameObjectWithTag(targetTag)) targetTransform = GameObject.FindGameObjectWithTag(targetTag).transform;
+        if(targetTransform)
+            NOOD.NoodyCustomCode.LerpSmoothCameraFollow(Camera.main.gameObject, smoothTime, targetTransform, offset);
+        //this.transform.LookAt(targetTransform);
+    }
     
     public void Shake(){
         StartCoroutine(NOOD.NoodyCustomCode.ObjectShake(this.gameObject, duration, magnitude));
     }
 
-    public void ExplodeShake(){
+    public void HeaveShake(){
         StartCoroutine(NOOD.NoodyCustomCode.ObjectShake(this.gameObject, duration, explodeMagnitude));
     }
 }
