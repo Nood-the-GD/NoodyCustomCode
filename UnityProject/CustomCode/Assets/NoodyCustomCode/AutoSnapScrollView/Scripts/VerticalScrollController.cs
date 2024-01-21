c5f84a933e574ad4bc7f735c87fcfca236df9dc4
tuong-student <62384988+tuong-student@users.noreply.github.com>
2024-01-21 16:54:03 +0700

    add auto snap scroll view

---
+++ UnityProject/CustomCode/Assets/NoodyCustomCode/AutoSnapScrollView/Scripts/VerticalScrollController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class VerticalScrollController : MonoBehaviour, IBeginDragHandler, IEndDragHandler
+{
+    [SerializeField] private ScrollRect _scrollRect;
+    [SerializeField] private RectTransform _contentPanel;
+    [SerializeField] private RectTransform _sampleListItem;
+    [SerializeField] private VerticalLayoutGroup _verticalLayoutG;
+
+    bool _isDrag;
+    bool _isSnapped = false;
+    [SerializeField] private float _snappingForce = 1;
+    float _snappingSpeed;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _isDrag = true;
+        _snappingSpeed = 0;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _isDrag = false;
+        _isSnapped = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int currentItemIndex = Mathf.RoundToInt((0 - _contentPanel.localPosition.y / (_sampleListItem.rect.height + _verticalLayoutG.spacing)));
+
+        Vector3 contentPanelPos = _contentPanel.localPosition;
+        float currentScrollSpeed = _scrollRect.velocity.magnitude;
+
+        if(_isDrag == false && _isSnapped == false && currentScrollSpeed < 200)
+        {
+            _scrollRect.velocity = Vector2.zero;
+            _snappingSpeed += _snappingForce * Time.deltaTime;
+
+            float targetY = 0 - (currentItemIndex * (_sampleListItem.rect.height + _verticalLayoutG.spacing));
+
+            float newY = Mathf.MoveTowards(_contentPanel.localPosition.y, targetY, _snappingSpeed);
+            _contentPanel.localPosition = new Vector3(contentPanelPos.x, newY, contentPanelPos.z);
+
+            if (_contentPanel.localPosition.y == targetY)
+                _isSnapped = true;
+        }
+    }
+}
